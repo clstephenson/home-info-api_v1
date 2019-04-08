d2b7707777fedf3f6e5b93a4453055c016209c29
Chris Stephenson <clstephenson@gmail.com>
2019-04-08 15:02:41 -0700

    Add entity for Vendor

---
+++ src/main/java/com/clstephenson/homeinfo/api_v1/model/Vendor.java
@@ -0,0 +1,79 @@
+package com.clstephenson.homeinfo.api_v1.model;
+
+import org.hibernate.validator.constraints.URL;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Size(max = 10)
+    private String phone;
+
+    @Email
+    private String email;
+
+    @URL
+    private String website;
+
+    private String notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
