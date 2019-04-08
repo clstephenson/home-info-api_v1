07c4bb7a92f2c2f908cdc47804e632dfad7a4df5
Chris Stephenson <clstephenson@gmail.com>
2019-04-08 16:56:27 -0700

    Add entity for StoredFile

---
+++ src/main/java/com/clstephenson/homeinfo/api_v1/model/StoredFile.java
@@ -0,0 +1,57 @@
+package com.clstephenson.homeinfo.api_v1.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity(name = "file")
+public class StoredFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "property_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Property property;
+    private String path;
+    private FileCategory category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public FileCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(FileCategory category) {
+        this.category = category;
+    }
+
+    public enum FileCategory {
+        DOCUMENT,
+        PHOTO
+    }
+}
