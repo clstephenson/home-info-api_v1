c6950acaaeb7b006526c5026bdeb3d0d10f7cbb8
Chris Stephenson <clstephenson@gmail.com>
2019-04-08 17:06:09 -0700

    Add entity for Color

---
+++ src/main/java/com/clstephenson/homeinfo/api_v1/model/Color.java
@@ -0,0 +1,79 @@
+package com.clstephenson.homeinfo.api_v1.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+public class Color {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "property_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Property property;
+
+    private String name;
+
+    private String manufacturer;
+
+    @Column(name = "color_code")
+    private String colorCode;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private Location location;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
