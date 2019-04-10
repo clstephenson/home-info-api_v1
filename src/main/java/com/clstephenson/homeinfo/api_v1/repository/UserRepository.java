f3811e2f75df28e8a0d582cc0c3b5dc571fd7b0e
Chris Stephenson <clstephenson@gmail.com>
2019-04-09 17:04:22 -0700

    Add method findByEmail

--- src/main/java/com/clstephenson/homeinfo/api_v1/repository/UserRepository.java
+++ src/main/java/com/clstephenson/homeinfo/api_v1/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.clstephenson.homeinfo.api_v1.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByEmail(String email);
 }
